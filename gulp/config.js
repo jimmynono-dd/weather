3bb7add8786e07b34d4434e6ae54443968f81f5f
James Norton <james.norton@me.com>
2016-01-15 15:51:52 -0800

    Initialize Repo, add gulp files

---
+++ gulp/config.js
@@ -0,0 +1,45 @@
+var dest = "./build";
+var src = './app';
+
+module.exports = {
+  javascript: {
+    src: src + '/js/**/*.js',
+    dest: dest + '/js/',
+    entry: src + '/js/entry.js',
+    outputFilename: 'main.js'
+  },
+  assets: {
+    src: src + "/assets/**/*",
+    dest: dest + '/assets/'
+  },
+  sass: {
+    src: src + "/styles/**/*.{sass,scss}",
+    dest: dest + '/styles/',
+    settings: {
+      indentedSyntax: true, // Enable .sass syntax!
+    }
+  },
+  fonts: {
+    src: src + '/styles/fonts/*',
+    dest: dest + "/styles/fonts/",
+    extensions: ['woff2', 'woff', 'eot', 'ttf', 'svg']
+  },
+  html: {
+    src: src + '/**/*.html',
+    dest: dest
+  },
+  server: {
+    src: dest,
+    host: '0.0.0.0',
+    livereload: true,
+    directoryListing: false,
+    open: false,
+    port: 9000
+  },
+  production: {
+    cssSrc: dest + '/css/*.css',
+    jsSrc: dest + '/js/*.js',
+    cssDest: dest + '/css/',
+    jsDest: dest + '/js/',
+  }
+};
---
+++ gulp/tasks/default.js
@@ -0,0 +1,4 @@
+var gulp = require('gulp');
+
+gulp.task('default', ['sass', 'markup', 'html', 'fonts', 'javascript', 'webpack', 'watch', 'serve']);
+
