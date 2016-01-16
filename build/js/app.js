f004ccce36ad112e974a26a6e15b9a99469b9c9d
James Norton <james.norton@me.com>
2016-01-16 11:43:23 -0800

    delete percent of rain, add wind direction and change kps to mph

--- build/js/app.js
+++ build/js/app.js
@@ -1,16 +1,17 @@
-console.log('james is not smart');
-
 var app = angular.module('WeatherApp', []);
 
 app.controller('WeatherAppController', function($scope, $http) {
   $http({
     method:'GET',
-    url: 'http://api.openweathermap.org/data/2.5/weather?q=Seattlel&appid=f3b7c0e02a9e45674adf168e76fcb6ee'
+    url: 'http://api.openweathermap.org/data/2.5/weather?q=Seattlel&units=imperial&appid=f3b7c0e02a9e45674adf168e76fcb6ee'
   })
   .success(function(response) {
     console.log(response);
+    $scope.date = new Date();
     $scope.city = response.name;
-    $scope.temperature = response.main;
+    $scope.temp = response.main.temp;
+    $scope.wind = response.wind.speed;
+    $scope.humidity = response.main.humidity;
   });
 
   // $scope.message = "88";
--- build/js/main.js
+++ build/js/main.js
@@ -29088,15 +29088,16 @@
 	app.controller('WeatherAppController', function($scope, $http) {
 	  $http({
 	    method:'GET',
-	    url: 'http://api.openweathermap.org/data/2.5/weather?q=Seattlel&units=imperial&appid=f3b7c0e02a9e45674adf168e76fcb6ee'
+	    url: 'http://api.openweathermap.org/data/2.5/weather?q=seattle&units=imperial&appid=f3b7c0e02a9e45674adf168e76fcb6ee'
 	  })
 	  .success(function(response) {
 	    console.log(response);
 	    $scope.date = new Date();
 	    $scope.city = response.name;
 	    $scope.temp = response.main.temp;
-	    $scope.wind = response.wind.speed;
+	    $scope.wind = response.wind.speed  * 2.23694;
 	    $scope.humidity = response.main.humidity;
+	    $scope.windDirection = response.wind.deg;
 	  });
 
 	  // $scope.message = "88";
